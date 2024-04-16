8ad20728bb6b2f406ca5e38b8b96513360a6b3d6
송경후 <fmaxgo@song-gyeonghuui-noteubug.local>
2024-04-16 19:32:57 +0900

    스크롤 반응성 수정 디자인 변경

--- src/Pages/Main/Components/MainFont.tsx
+++ src/Pages/Main/Components/MainFont.tsx
@@ -1,18 +1,22 @@
 import {styled, keyframes} from "styled-components"
-import { useState, useEffect } from "react";
-import { flexCenter } from "../../../Styles/GlobalStyle";
-interface Font{
-    fontSize:number;
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+export interface Font{
+    scroll:number;
 }
 
 export const MainFont = () => {
-    const [fontSize, setFontSize] = useState(64)
+    const dispatch = useDispatch()
+    const scroll = useSelector((state:any) => state.scroll)
     useEffect(() => {
         const handleScroll = () => {
           const scrollTop = window.scrollY;
-          console.log(scrollTop)
-          // 스크롤을 아래로 내릴수록 글자를 크게 만듭니다.
-          setFontSize(64 + scrollTop*2); // 10은 조절 가능한 스크롤 속도입니다.
+          dispatch (
+                {
+                    type: 'SCROLL',
+                    scrollAmt: (scrollTop*2)
+                }
+            )
         }
     
         window.addEventListener('scroll', handleScroll)
@@ -22,7 +26,7 @@ export const MainFont = () => {
         }
       }, [])
     return (
-        <FontDiv fontSize={fontSize}>
+        <FontDiv scroll={scroll+64}>
             <h1 className='empty'>HI, I'M</h1>
             <h1 className='empty'>FRONTEND</h1>
             <h1 className='empty'>DEVELOPER</h1>
@@ -48,17 +52,15 @@ const FontDiv = styled.div<Font>`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    display: ${props => props.fontSize < 1400 ? 'flex' : 'none'};
+    display: ${props => props.scroll < 1400 ? 'flex' : 'none'};
     justify-content: center;
     align-items: center;
     flex-direction: column;
     width: 100%;
-    /* animation: ${moveUpDown} 2s ease-in-out infinite; */
-    /* transition: font-size 0.5s ease; */
-    font-size:${props => props.fontSize}px;
+    font-size:${props => props.scroll}px;
     overflow: hidden;
     h1{
-        /* line-height: ${props => props.fontSize + 25}px; */
+        /* line-height: ${props => props.scroll + 25}px; */
         margin: 0;
         padding:0;
         @media(max-width: 600px){
