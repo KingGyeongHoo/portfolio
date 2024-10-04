e92386f5abc14df4a22e66c266fd1c0551cd5d0a
KINGGH <fmaxgo@naver.com>
2024-10-04 11:48:30 +0900

    Skill 추가

--- src/Data/SkillData.tsx
+++ src/Data/SkillData.tsx
@@ -1,70 +1,74 @@
-export interface Skills{
-    skill: string;
-    url:string;
+export interface Skills {
+  skill: string;
+  url: string;
 }
-export const SkillData:Skills[] = [
-    {
-        skill:'JAVASCRIPT',
-        url:'javascript'
-    },
-    {
-        skill:'TYPESCRIPT',
-        url:'typescript'
-    },
-    {
-        skill:'REACT',
-        url:'react'
-    },
-    {
-        skill:'HTML',
-        url:'html5'
-    },
-    {
-        skill:'CSS',
-        url:'css3'
-    },
-    {
-        skill:'STYLED-COMPONENTS',
-        url:'styledcomponents'
-    },
-    {
-        skill:'SCSS',
-        url:'sass'
-    },
-    {
-        skill:'REDUX',
-        url:'redux'
-    },
-    {
-        skill:'RECOIL',
-        url:'recoil'
-    },
-    {
-        skill:'MOBX',
-        url:'mobx'
-    },
-    {
-        skill:'AXIOS',
-        url:'axios'
-    },
-    {
-        skill:'NODE.JS',
-        url:'nodedotjs'
-    },
-    {
-        skill:'EXPRESS.JS',
-        url:'express'
-    },
-    {
-        skill:'GIT',
-        url:'git'
-    },
-    {
-        skill:'FIGMA',
-        url:'figma'
-    },
-    {
-        skill:'PHOTOSHOP',
-        url:'adobephotoshop'
-    }
-]
+export const SkillData: Skills[] = [
+  {
+    skill: "JAVASCRIPT",
+    url: "javascript",
+  },
+  {
+    skill: "TYPESCRIPT",
+    url: "typescript",
+  },
+  {
+    skill: "REACT",
+    url: "react",
+  },
+  {
+    skill: "NEXT.JS",
+    url: "nextdotjs",
+  },
+  {
+    skill: "HTML",
+    url: "html5",
+  },
+  {
+    skill: "CSS",
+    url: "css3",
+  },
+  {
+    skill: "STYLED-COMPONENTS",
+    url: "styledcomponents",
+  },
+  {
+    skill: "SCSS",
+    url: "sass",
+  },
+  {
+    skill: "TAILWIND CSS",
+    url: "tailwindcss",
+  },
+  {
+    skill: "REDUX",
+    url: "redux",
+  },
+  {
+    skill: "RECOIL",
+    url: "recoil",
+  },
+  {
+    skill: "AXIOS",
+    url: "axios",
+  },
+  {
+    skill: "NODE.JS",
+    url: "nodedotjs",
+  },
+  {
+    skill: "EXPRESS.JS",
+    url: "express",
+  },
+  {
+    skill: "GIT",
+    url: "git",
+  },
+  {
+    skill: "FIGMA",
+    url: "figma",
+  },
+  {
+    skill: "PHOTOSHOP",
+    url: "adobephotoshop",
+  },
+];
