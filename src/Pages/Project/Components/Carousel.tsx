4b4ffef48df8dc6bbc6f2f6f9c66546547ec6be3
KINGGH <fmaxgo@naver.com>
2024-11-13 14:49:21 +0900

    refactor: 프로젝트 데이터 추가 및 버그, 오타 수정

--- src/Pages/Project/Components/Carousel.tsx
+++ src/Pages/Project/Components/Carousel.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled, {css, keyframes} from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import { useInView } from "react-intersection-observer";
 import ProjectCard from "./ProjectCard";
 import { projectData, Project } from "../../../Data/projectData";
@@ -9,63 +9,76 @@ import { flexCenter } from "../../../Styles/GlobalStyle";
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { FaArrowCircleRight } from "react-icons/fa";
 
-interface IsShown{
-    isShown:boolean;
-  }
+interface IsShown {
+  isShown: boolean;
+}
 interface Direction {
-    dir: string;
+  dir: string;
 }
 interface Left {
-    idx: number;
+  idx: number;
 }
 interface IsSelected {
-    isSelected: boolean;
+  isSelected: boolean;
 }
 
 export const Carousel = () => {
-    const dispatch = useDispatch()
-    const idx = useSelector((state: any) => state.selectedIdx)
+  const dispatch = useDispatch();
+  const idx = useSelector((state: any) => state.selectedIdx);
 
-    useEffect(() => {
-        if (idx <= 0) {
-            dispatch({
-                type: '0'
-            })
-        } else if (idx >= projectData.length - 1) {
-            dispatch({
-                type: (projectData.length - 1).toString()
-            })
-        }
-    }, [idx])
-    const { ref, inView } = useInView({
-        threshold: 0.5
+  useEffect(() => {
+    if (idx <= 0) {
+      dispatch({
+        type: "0",
       });
-
-    const MoveButton = ({ dir }: Direction) => {
-        return (
-            <MoveButtonDiv dir={dir}>
-                {dir === 'left' ? (
-                    <FaArrowCircleLeft size='50' onClick={() => dispatch({ type: (idx - 1).toString() })} />
-                ) : (
-                    <FaArrowCircleRight size='50' onClick={() => dispatch({ type: (idx + 1).toString() })} />
-                )}
-            </MoveButtonDiv>
-        )
+    } else if (idx >= projectData.length - 1) {
+      dispatch({
+        type: (projectData.length - 1).toString(),
+      });
     }
+  }, [idx]);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
 
+  const MoveButton = ({ dir }: Direction) => {
     return (
-        <CarouselContainer ref={ref} isShown={inView}>
-            <MoveButton dir='left'></MoveButton>
-            <CarouselDiv idx={idx}>
-                {projectData.map((el: Project, index: number) => <ProjectCard key={index} content={el} idx={index}></ProjectCard>)}
-            </CarouselDiv>
-            <MoveButton dir='right'></MoveButton>
-            <ItemIndexDiv>
-                {Array.from(projectData, (el, index) => <ItemIndex isSelected={index === idx} onClick={() => dispatch({ type: index.toString() })}></ItemIndex>)}
-            </ItemIndexDiv>
-        </CarouselContainer>
-    )
-}
+      <MoveButtonDiv dir={dir}>
+        {dir === "left" ? (
+          <FaArrowCircleLeft
+            size="50"
+            onClick={() => dispatch({ type: (idx - 1).toString() })}
+          />
+        ) : (
+          <FaArrowCircleRight
+            size="50"
+            onClick={() => dispatch({ type: (idx + 1).toString() })}
+          />
+        )}
+      </MoveButtonDiv>
+    );
+  };
+
+  return (
+    <CarouselContainer ref={ref} isShown={inView}>
+      <MoveButton dir="left"></MoveButton>
+      <CarouselDiv idx={idx}>
+        {projectData.map((el: Project, index: number) => (
+          <ProjectCard key={index} content={el} idx={index}></ProjectCard>
+        ))}
+      </CarouselDiv>
+      <MoveButton dir="right"></MoveButton>
+      <ItemIndexDiv>
+        {Array.from(projectData, (el, index) => (
+          <ItemIndex
+            isSelected={index === idx}
+            onClick={() => dispatch({ type: index.toString() })}
+          ></ItemIndex>
+        ))}
+      </ItemIndexDiv>
+    </CarouselContainer>
+  );
+};
 
 export const fadeInAnimation = keyframes`
   from {
@@ -79,71 +92,72 @@ export const fadeInAnimation = keyframes`
 `;
 
 const CarouselContainer = styled.div<IsShown>`
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 100%;
-    height: 80%;
-    margin-top: 2%;
-    opacity: 0;
-    background-color: rgba(0,0,0,0);
-    box-shadow: 0px 2px 10px rgba(0,0,0,0.2);
-    overflow: hidden;
-    ${(props) =>
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+  margin-top: 2%;
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0);
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  ${(props) =>
     props.isShown &&
     css`
       animation: ${fadeInAnimation} 0.3s ease-in-out forwards;
     `};
-`
+`;
 const MoveButtonDiv = styled.div<Direction>`
-    position: absolute;
-    ${props => (props.dir === 'left' ? 'left: 1%' : 'right: 1%')};
-    opacity: 0.05;
-    z-index: 10;
-    cursor:pointer;
-    &:hover{
-        opacity: 0.1;
-    }
-`
+  position: absolute;
+  ${(props) => (props.dir === "left" ? "left: 1%" : "right: 1%")};
+  opacity: 0.05;
+  z-index: 10;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.1;
+  }
+`;
 
 const CarouselDiv = styled.div<Left>`
-    position: absolute;
-    top: 50%;
-    left:${({ idx }) => 34 - idx * 33.333}%;
-    @media(max-width : 600px){
-        left: ${({ idx }) => 15 - idx * 70}%;
-    }
-    display: flex;
-    flex-direction: row;
-    width: 132%;
-    @media(max-width : 600px){
-        width: 280%;
-    }
-    height: 80%;
-    align-items: center;
-    transition: left 0.5s ease-in-out;
-    transform: translate(0, -50%);
-`
+  position: absolute;
+  top: 50%;
+  left: ${({ idx }) => 34 - idx * 25}%;
+  @media (max-width: 600px) {
+    left: ${({ idx }) => 15 - idx * 70}%;
+  }
+  display: flex;
+  flex-direction: row;
+  width: 132%;
+  @media (max-width: 600px) {
+    width: 280%;
+  }
+  height: 80%;
+  align-items: center;
+  transition: left 0.5s ease-in-out;
+  transform: translate(0, -50%);
+`;
 const ItemIndexDiv = styled.div`
-    position: absolute;
-    bottom: 2.5%;
-    left: 50%;
-    transform: translate(-50%, 0);
-    ${flexCenter};
-    width: 40%;
-    height: 1.5%;
-    background-color: white;
-`
+  position: absolute;
+  bottom: 2.5%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  ${flexCenter};
+  width: 40%;
+  height: 1.5%;
+  background-color: white;
+`;
 const ItemIndex = styled.div<IsSelected>`
-    height: 100%;
-    aspect-ratio: 1 / 1;
-    margin: 0 1%;
-    transform: rotate(${({ isSelected }) => isSelected ? '45deg' : 0});
-    border: 1px solid ${({ theme }) => theme.fontColor.light_gray};
-    background-color: ${({ isSelected }) => isSelected ? ({ theme }) => theme.fontColor.light_gray : 'rgba(0,0,0,0)'};
-    transition: transform 0.5s ease;
-    opacity: 0.5;
-    z-index: 10;
-    cursor: pointer;
-`
+  height: 100%;
+  aspect-ratio: 1 / 1;
+  margin: 0 1%;
+  transform: rotate(${({ isSelected }) => (isSelected ? "45deg" : 0)});
+  border: 1px solid ${({ theme }) => theme.fontColor.light_gray};
+  background-color: ${({ isSelected }) =>
+    isSelected ? ({ theme }) => theme.fontColor.light_gray : "rgba(0,0,0,0)"};
+  transition: transform 0.5s ease;
+  opacity: 0.5;
+  z-index: 10;
+  cursor: pointer;
+`;
--- src/Pages/Project/Components/ProjectModal.tsx
+++ src/Pages/Project/Components/ProjectModal.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { Project, StacksProps, Functions } from "../../../Data/projectData";
+import { StacksProps, Functions } from "../../../Data/projectData";
 import { useSelector, useDispatch } from "react-redux";
-import { flexCenter, ContentDivider, highlight } from "../../../Styles/GlobalStyle";
+import { ContentDivider, highlight } from "../../../Styles/GlobalStyle";
 import { LuExternalLink } from "react-icons/lu";
 
 import { IoCloseSharp } from "react-icons/io5";
@@ -12,54 +12,51 @@ interface IsOpen {
 }
 
 const ProjectModal = () => {
-  const dispatch = useDispatch()
-  const isOpen = useSelector((state: any) => state.isOpen)
-  const project = useSelector((state: any) => state.project)
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state: any) => state.isOpen);
+  const project = useSelector((state: any) => state.project);
   const closeModal = () => {
     dispatch({
-      type: 'Modal_Close'
-    })
+      type: "Modal_Close",
+    });
+  };
+  const modalContent = document.querySelector(".modal") as HTMLElement;
+  if (modalContent !== null) {
+    modalContent.scrollTop = 0;
   }
-  const modalContent = document.querySelector('.modal') as HTMLElement;
-    if(modalContent !== null){
-        modalContent.scrollTop = 0;
-    }
 
-  const openLink = (e:any) => {
-    if(e.target.innerText === 'Github'){
-      if(project.github.length < 1){
-        alert('준비중입니다!')
+  const openLink = (e: any) => {
+    if (e.target.innerText === "Github") {
+      if (project.github.length < 1) {
+        alert("준비중입니다!");
       } else {
         window.open(`${project.github}`, "_blank");
       }
-      
-    } else if(e.target.innerText === 'Figma'){
-      if(project.figma.length < 1){
-        alert('준비중입니다!')
+    } else if (e.target.innerText === "Figma") {
+      if (project.figma.length < 1) {
+        alert("준비중입니다!");
       } else {
         window.open(`${project.figma}`, "_blank");
       }
-      
     } else {
-      if(project.deploy.length < 1){
-        alert('준비중입니다!')
+      if (project.deploy.length < 1) {
+        alert("준비중입니다!");
       } else {
         window.open(`${project.deploy}`, "_blank");
       }
-      
     }
-  }
+  };
   return (
     <ModalBackground onClick={closeModal} isOpen={isOpen}>
       <ModalContainer className="modal" onClick={(e) => e.stopPropagation()}>
         <div className="close">
           <div className="close-modal">
-            <IoCloseSharp size='36' onClick={closeModal}></IoCloseSharp>
+            <IoCloseSharp size="36" onClick={closeModal}></IoCloseSharp>
           </div>
         </div>
         <ContentDivider>
           <div className="title">
-            <GoDotFill size='24'></GoDotFill>
+            <GoDotFill size="24"></GoDotFill>
             <h2>프로젝트 개요</h2>
             <div></div>
           </div>
@@ -69,28 +66,26 @@ const ProjectModal = () => {
             <h3 className="date">{project.date}</h3>
           </div>
           <div className="describe">
-            <p>
-              {project.desc}
-            </p>
+            <p>{project.desc}</p>
           </div>
           <Links>
             <div onClick={openLink}>
               <span>Github</span>
-              <LuExternalLink color='#bbbbbb'></LuExternalLink>
+              <LuExternalLink color="#bbbbbb"></LuExternalLink>
             </div>
             <div onClick={openLink}>
               <span>배포 링크</span>
-              <LuExternalLink color='#bbbbbb'></LuExternalLink>
+              <LuExternalLink color="#bbbbbb"></LuExternalLink>
             </div>
             <div onClick={openLink}>
               <span>Figma</span>
-              <LuExternalLink color='#bbbbbb'></LuExternalLink>
+              <LuExternalLink color="#bbbbbb"></LuExternalLink>
             </div>
           </Links>
         </ContentDivider>
         <ContentDivider>
           <div className="title">
-            <GoDotFill size='24'></GoDotFill>
+            <GoDotFill size="24"></GoDotFill>
             <h2>Teck Stacks</h2>
             <div></div>
           </div>
@@ -99,22 +94,17 @@ const ProjectModal = () => {
               {project?.info?.stacks?.map((el: StacksProps, idx: number) => {
                 return (
                   <li key={idx}>
-                    <p className="stack-type">
-                      {el.type}
-                    </p>
-                    <p className="stack-stackname">
-                      {el.stack}
-                    </p>
+                    <p className="stack-type">{el.type}</p>
+                    <p className="stack-stackname">{el.stack.join(", ")}</p>
                   </li>
-                )
-              })
-              }
+                );
+              })}
             </TechUl>
           </div>
         </ContentDivider>
         <ContentDivider>
           <div className="title">
-            <GoDotFill size='24'></GoDotFill>
+            <GoDotFill size="24"></GoDotFill>
             <h2>My Role</h2>
             <div></div>
           </div>
@@ -122,33 +112,27 @@ const ProjectModal = () => {
             {project?.info?.functions?.map((el: Functions, idx: number) => {
               return (
                 <MyroleUl>
-                  <h3>
-                    {el.title}
-                  </h3>
+                  <h3>{el.title}</h3>
                   {el.function.map((content: string, idx: number) => {
-                    return <li>{content}</li>
+                    return <li>{content}</li>;
                   })}
                 </MyroleUl>
-              )
-            })
-            }
+              );
+            })}
           </MyroleDiv>
         </ContentDivider>
         <ContentDivider>
           <div className="title">
-            <GoDotFill size='24'></GoDotFill>
+            <GoDotFill size="24"></GoDotFill>
             <h2>느낀점</h2>
             <div></div>
           </div>
           <div className="describe">
-            {project?.info?.feeling?.split('.').map((el: string, idx: number) => {
-              return (
-                <p className="describe-lines">
-                  {el}
-                </p>
-              )
-            })
-            }
+            {project?.info?.feeling
+              ?.split("\n")
+              .map((el: string, idx: number) => {
+                return <p className="describe-lines">{el}</p>;
+              })}
           </div>
         </ContentDivider>
       </ModalContainer>
@@ -157,115 +141,115 @@ const ProjectModal = () => {
 };
 
 const ModalBackground = styled.div<IsOpen>`
-    position: fixed;
-    top: 0;
-    left: 0;
-    /* display: ${({ isOpen }) => isOpen ? 'flex' : 'none'}; */
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.35);
-    opacity: ${({ isOpen }) => isOpen ? '1' : '0'};
-    z-index: ${({ isOpen }) => isOpen ? '15' : '-15'};
-    transition: opacity 0.3s ease-in-out;
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  /* display: ${({ isOpen }) => (isOpen ? "flex" : "none")}; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.35);
+  opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "15" : "-15")};
+  transition: opacity 0.3s ease-in-out;
+`;
 const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 75%;
   height: 80%;
-  padding:3%;
+  padding: 3%;
   background-color: white;
   border-radius: 10px;
-  overflow-y:scroll;
-  .close{
+  overflow-y: scroll;
+  .close {
     width: 100%;
     display: flex;
     justify-content: flex-end;
-    div.close-modal{
+    div.close-modal {
       cursor: pointer;
     }
   }
-`
+`;
 
 const Links = styled.div`
   width: 90%;
   display: flex;
   flex-direction: row;
-  div{
+  div {
     display: flex;
     flex-direction: row;
     align-items: center;
     margin-right: 5%;
-    span{
+    span {
       margin-right: 2px;
       font-size: 21px;
-      @media(max-width : 600px){
+      @media (max-width: 600px) {
         font-size: 12px;
       }
-      color: ${({theme}) => theme.fontColor.light_gray};
+      color: ${({ theme }) => theme.fontColor.light_gray};
       text-decoration: underline;
       cursor: pointer;
     }
   }
-`
+`;
 
 const TechUl = styled.ul`
   width: 100%;
   margin-bottom: 1.5%;
-  li{
+  li {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     width: 100%;
     margin: 1% 0;
-    p{
+    p {
       font-size: 27px;
-      @media(max-width : 600px){
+      @media (max-width: 600px) {
         font-size: 12px;
       }
       overflow-wrap: break-word;
     }
-    p.stack-type{
+    p.stack-type {
       width: 12%;
       text-align: right;
       font-weight: 900;
     }
-    p.stack-stackname{
+    p.stack-stackname {
       width: 85%;
       text-align: left;
       color: ${({ theme }) => theme.fontColor.dark_gray};
       font-weight: 500;
     }
   }
-`
+`;
 const MyroleDiv = styled.div`
   display: flex;
   flex-direction: column;
   width: 90%;
   margin: 1.5% 0;
-`
+`;
 const MyroleUl = styled.ul`
   width: 100%;
   margin-bottom: 1.5%;
-  h3{
+  h3 {
     ${highlight};
     margin-left: -2%;
-    font-size:24px;
-    @media(max-width : 600px){
+    font-size: 24px;
+    @media (max-width: 600px) {
       font-size: 16px;
     }
   }
-  li{
+  li {
     margin: 1% 0;
     font-size: 20px;
-    @media(max-width : 600px){
+    @media (max-width: 600px) {
       font-size: 14px;
     }
   }
-`
+`;
 
-export default ProjectModal
+export default ProjectModal;
